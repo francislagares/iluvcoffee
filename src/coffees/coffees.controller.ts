0f02d6ee6bdbb1acdf32419e5955bac7aac0479c
Francis Lagares <francis.lagares@gmail.com>
2022-12-25 14:14:22 +0100

    feat: coffees CRUD module

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,45 @@
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+
+    return this.coffeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeeService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeeService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+  }
+}
